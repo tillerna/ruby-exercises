5da66abd1c0ce52a558a56732c191ed09f78f502
Nate Tiller <tillerna@gmail.com>
2022-07-23 16:38:22 -0400

    Completed through Exercise 9

--- ruby_basics/9_debugging/exercises/debugging_exercises.rb
+++ ruby_basics/9_debugging/exercises/debugging_exercises.rb
@@ -10,7 +10,7 @@ def decrement_smallest_value(nested_array)
   nested_array.each do |array|
     array.each do |current_value|
       if smallest_value > current_value
-        smallest_value = current_valu
+        smallest_value = current_value
       end
     end
   end
@@ -24,7 +24,7 @@ def increment_greatest_value(nested_array)
   greatest_value = nested_array.flatten.min
   nested_array.each do |array|
     array.each do |current_value|
-      if greatest_value < nil
+      if greatest_value < current_value
         greatest_value = current_value
       end
     end
@@ -36,10 +36,9 @@ end
 # Use p and puts in order to find what's wrong with our method
 
 def isogram?(string)
-  original_length = string.length
   string_array = string.downcase.split
-  unique_length = string_array.uniq.length
-  original_length == unique_length
+  unique = string_array.uniq
+  return string_array[0] == unique[0]
 end
 
 # Can you guess what's next?
@@ -50,7 +49,6 @@ end
 # Once you find the error, fix it and get the test to pass
 
 def yell_greeting(string)
-  name = string
-  name = name.downcase
+  name = string.upcase
   greeting = "WASSAP, #{name}!"
 end
