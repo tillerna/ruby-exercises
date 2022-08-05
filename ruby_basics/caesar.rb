055ccd470a468d44c52501e5e0fda4eafaae0254
Nate Tiller <tillerna@gmail.com>
2022-08-05 10:00:02 -0400

    Fixed shift factor >26

--- ruby_basics/caesar.rb
+++ ruby_basics/caesar.rb
@@ -2,20 +2,22 @@ def caesar_cipher(string, shift)
     alpha_array = 'abcdefghijklmnopqrstuvwxyz'.split("")
     string_array = string.split("")
     result = []
-
+    
     i = 0
     until i == string.length
         letter = string_array[i]
-        if alpha_array.any?(letter) == true
-            # need to make sure shift always cons to be <= 24, use modulus for remainder (%26)
-        alpha_idx = alpha_array.find_index(letter) + shift
+        alpha_idx = alpha_array.find_index(letter).to_i
+        shift_idx = (alpha_idx + shift) % 26 # handles any type of shift factor above the length of the alphabet
+
+        if alpha_array.any?(letter) == true # checks to see if letter is in alpha_array
             if letter != letter.capitalize
-                result << alpha_array[alpha_idx].downcase
+                result << alpha_array[shift_idx] # if it isn't capitalized, use the letter IN alpha_array
             elsif letter == letter.capitalize
-                 result << alpha_array[alpha_idx].capitalize
+                 result << alpha_array[shift_idx] # if it is, do something to make it capital
             end
-        elsif result << letter
+        elsif result << letter # otherwise, this just passes the character along (!@#$%, etc.)
         end
+
         i+=1
     end
 
@@ -28,10 +30,10 @@ end
 # puts "Enter a shift factor:"
 # shift_factor = gets.chomp.to_i
 
-phrase = "z"
-shift_factor = 1
+phrase = "TeStInG!@# $%^THIS *)(out"
+shift_factor = 27
 
 puts "The new phrase is: " + caesar_cipher(phrase, shift_factor)
 
-puts 27 % 26
+
 
